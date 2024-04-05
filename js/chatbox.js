6a54ca62873ab5b4b3b46828d98d7c30b516abdd
Nin Lê <lengocnin96@outlook.com>
2024-04-05 14:40:43 +0700

    update

--- js/chatbox.js
+++ js/chatbox.js
@@ -1,13 +1,21 @@
+var button = document.getElementById("showPopupButton");
 var btnChat = document.getElementById("start-chat-btn");
 var btnSend = document.getElementById("btn-send");
 var btnClose = document.getElementById("close-btn");
-var button = document.getElementById("showPopupButton");
 
-var form = document.getElementById("form");
+var chatContent = document.getElementById("chat-content");
+
 var popup = document.getElementById("popup");
+var helloMess = document.getElementById("hello-mess");
 var newMess = document.getElementById("new-mess");
+var thanksMess = document.getElementById("thanks-mess");
 
 button.addEventListener("click", function () {
+    setTimeout(() => {
+        helloMess.classList.remove("hidden");
+        helloMess.classList.add("visible");
+    }, 800);
+
     if (popup.classList.contains("hidden")) {
         popup.classList.remove("hidden");
         popup.classList.add("visible");
@@ -16,20 +24,32 @@ button.addEventListener("click", function () {
         popup.classList.add("hidden");
     }
 
-    if (form.classList.contains("visible")) {
-        form.classList.remove("visible");
-        form.classList.add("hidden");
-    }
-
     if (popup.classList.contains("visible")) {
         setTimeout(() => {
             button.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-            button.classList.remove("change-color");
+            button.classList.add("change-color");
         }, 400);
     } else {
         button.innerHTML = '<i class="message-icon fa-solid fa-comments"></i>';
-        button.classList.add("change-color");
+        button.classList.remove("change-color");
     }
+
+    if (thanksMess.classList.contains("visible")) {
+        thanksMess.classList.remove("visible");
+        thanksMess.classList.add("hidden");
+
+        newMess.classList.remove("visible");
+        newMess.classList.add("hidden");
+    }
+
+    if (newMess.classList.contains("hidden")) {
+        btnChat.classList.remove("hidden");
+        btnChat.classList.add("visible");
+    }
+
+    // if (popup.classList.contains("popup-mini")) {
+    //     popup.classList.remove("popup-mini");
+    // }
 });
 
 btnChat.addEventListener("click", function () {
@@ -38,7 +58,7 @@ btnChat.addEventListener("click", function () {
             newMess.classList.remove("hidden");
             newMess.classList.add("visible");
             btnChat.classList.add("hidden");
-        }, 400);
+        }, 1000);
     }
 
     if (btnChat.classList.contains("visible")) {
@@ -47,20 +67,57 @@ btnChat.addEventListener("click", function () {
             btnChat.classList.add("hidden");
         }, 400);
     }
-});
 
-btnSend.addEventListener("click", function () {
-    if (newMess.classList.contains("visible")) {
-        newMess.classList.remove("visible");
-        newMess.classList.add("hidden");
-
-        btnChat.classList.remove("hidden");
-        btnChat.classList.add("visible");
+    if (thanksMess.classList.contains("visible")) {
+        setTimeout(() => {
+            thanksMess.classList.remove("visible");
+            thanksMess.classList.add("hidden");
+        }, 400);
     }
 });
 
+btnSend.addEventListener("click", function () {
+    // if (newMess.classList.contains("visible")) {
+    //     newMess.classList.remove("visible");
+    //     newMess.classList.add("hidden");
+
+    // btnChat.classList.remove("hidden");
+    // btnChat.classList.add("visible");
+
+    setTimeout(() => {
+        thanksMess.classList.remove("hidden");
+        thanksMess.classList.add("visible");
+        chatContent.scrollTop = chatContent.scrollHeight;
+    }, 2000);
+
+    btnChat.classList.remove("visible");
+    btnChat.classList.add("hidden");
+
+    // popup.classList.add("popup-mini");
+    // }
+
+    // thanksMess.classList.remove("hidden");
+    // thanksMess.classList.add("visible");
+});
+
 btnClose.addEventListener("click", function () {
+    if (thanksMess.classList.contains("visible")) {
+        thanksMess.classList.remove("visible");
+        thanksMess.classList.add("hidden");
+
+        newMess.classList.remove("visible");
+        newMess.classList.add("hidden");
+    }
+
+    if (popup.classList.contains("popup-mini")) {
+        popup.classList.remove("popup-mini");
+    }
+
     popup.classList.remove("visible");
     popup.classList.add("hidden");
+
     button.innerHTML = '<i class="message-icon fa-solid fa-comments"></i>';
+
+    thanksMess.classList.remove("visible");
+    thanksMess.classList.add("hidden");
 });
