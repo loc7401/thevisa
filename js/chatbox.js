1c417571df6d36d4b4c4f00f8104cdb0d2e252d6
Nin Lê <lengocnin96@outlook.com>
2024-04-08 16:37:33 +0700

    update

--- js/chatbox.js
+++ js/chatbox.js
@@ -1,124 +1,69 @@
 $(document).ready(function () {
-    setTimeout(() => {
-        $("#popup").removeClass("hidden");
-        $("#popup").addClass("visible");
+    function showPopup() {
+        $("#popup").removeClass("hidden").addClass("visible");
         $("#showPopupButton").html('<i class="fa-solid fa-xmark"></i>');
-
         setTimeout(() => {
-            $("#hello-mess").removeClass("hidden");
-            $("#hello-mess").addClass("visible");
+            $("#hello-mess").removeClass("hidden").addClass("visible");
         }, 800);
-    }, 10000);
+    }
 
-    $("#showPopupButton").on("click", function () {
-        setTimeout(() => {
-            $("#hello-mess").removeClass("hidden");
-            $("#hello-mess").addClass("visible");
-        }, 800);
+    function hidePopup() {
+        $("#popup").removeClass("visible").addClass("hidden");
+        $("#showPopupButton").html('<i class="message-icon fa-solid fa-comments"></i>');
+    }
 
+    function togglePopup() {
         if ($("#popup").hasClass("hidden")) {
-            $("#popup").removeClass("hidden");
-            $("#popup").addClass("visible");
-        } else {
-            $("#popup").removeClass("visible");
-            $("#popup").addClass("hidden");
-        }
-
-        if ($("#popup").hasClass("visible")) {
+            showPopup();
             setTimeout(() => {
                 $("#showPopupButton").html('<i class="fa-solid fa-xmark"></i>');
-                // $("#showPopupButton").addClass("change-color");
             }, 400);
         } else {
-            $("#showPopupButton").html('<i class="message-icon fa-solid fa-comments"></i>');
-            // $("#showPopupButton").removeClass("change-color");
+            hidePopup();
         }
-
         if ($("#good-mess").hasClass("visible")) {
-            $("#good-mess").removeClass("visible");
-            $("#good-mess").addClass("hidden");
-
-            $("#thanks-mess").removeClass("visible");
-            $("#thanks-mess").addClass("hidden");
-
-            $("#new-mess").removeClass("visible");
-            $("#new-mess").addClass("hidden");
-
-            $("#chat-content").removeClass("change-height");
+            resetMessages();
         }
+    }
 
-        if ($("#new-mess").hasClass("hidden")) {
-            $("#start-chat-btn").removeClass("hidden");
-            $("#start-chat-btn").addClass("visible");
-        }
+    function resetMessages() {
+        $("#new-mess").removeClass("visible").addClass("hidden");
+        $("#thanks-mess").removeClass("visible").addClass("hidden");
+        $("#good-mess").removeClass("visible").addClass("hidden");
+        $("#chat-content").removeClass("change-height");
+        $("#start-chat-btn").removeClass("hidden").addClass("visible");
+    }
 
-        // if ($("#thanks-mess").hasClass("visible")) {
-        //     $("#chat-content").removeClass("change-height");
-        // }
-    });
+    setTimeout(showPopup, 10000);
+
+    $("#showPopupButton").on("click", togglePopup);
 
     $("#close-btn").on("click", function () {
+        hidePopup();
         if ($("#good-mess").hasClass("visible")) {
-            $("#good-mess").removeClass("visible");
-            $("#good-mess").addClass("hidden");
-
-            $("#thanks-mess").removeClass("visible");
-            $("#thanks-mess").addClass("hidden");
-
-            $("#new-mess").removeClass("visible");
-            $("#new-mess").addClass("hidden");
-
-            $("#chat-content").removeClass("change-height");
+            resetMessages();
         }
-
-        $("#popup").removeClass("visible");
-        $("#popup").addClass("hidden");
-
-        $("#showPopupButton").html('<i class="message-icon fa-solid fa-comments"></i>');
     });
 
     $("#start-chat-btn").on("click", function () {
-        if ($("#new-mess").hasClass("hidden")) {
-            setTimeout(() => {
-                $("#new-mess").removeClass("hidden");
-                $("#new-mess").addClass("visible");
+        setTimeout(function () {
+            $("#new-mess").removeClass("hidden").addClass("visible");
+            $("#start-chat-btn").removeClass("visible");
+            $("#start-chat-btn").addClass("hidden");
 
-                $("#start-chat-btn").addClass("hidden");
-            }, 600);
-        }
-
-        if ($("#start-chat-btn").hasClass("visible")) {
-            setTimeout(() => {
-                $("#start-chat-btn").removeClass("visible");
-                $("#start-chat-btn").addClass("hidden");
-                $("#chat-content").addClass("change-height");
-            }, 400);
-        }
-
-        if ($("#thanks-mess").hasClass("visible")) {
-            setTimeout(() => {
-                $("#thanks-mess").removeClass("visible");
-                $("#thanks-mess").addClass("hidden");
-            }, 400);
-        }
+            $("#chat-content").addClass("change-height");
+        }, 400);
     });
 
     $("#btn-send").on("click", function () {
-        if ($("#thanks-mess").hasClass("hidden")) {
-            setTimeout(() => {
-                $("#thanks-mess").removeClass("hidden");
-                $("#thanks-mess").addClass("visible");
-                $("#chat-content").scrollTop($("#chat-content")[0].scrollHeight);
-                setTimeout(() => {
-                    $("#good-mess").addClass("visible");
-                    $("#chat-content").scrollTop($("#chat-content")[0].scrollHeight);
-                }, 1000);
-            }, 1500);
-        } else {
-            $("#thanks-mess").addClass("");
-        }
+        setTimeout(function () {
+            $("#thanks-mess").removeClass("hidden").addClass("visible");
+            $("#chat-content").scrollTop($("#chat-content")[0].scrollHeight);
 
-        $("#start-chat-btn").removeClass("visible");
-        $("#start-chat-btn").addClass("hidden");
+            setTimeout(function () {
+                $("#good-mess").addClass("visible");
+                $("#chat-content").scrollTop($("#chat-content")[0].scrollHeight);
+            }, 1000);
+        }, 1500);
     });
 });
