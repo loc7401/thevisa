d3a2168a3c6c90b977e0b30d0eb8266960e6b9a9
loc7401 <locvo.7401@gmail.com>
2024-04-09 16:04:41 +0700

    update

--- js/chatbox.js
+++ js/chatbox.js
@@ -1,45 +1,56 @@
 $(document).ready(function () {
+    setTimeout(showPopup, 10000);
+
     function showPopup() {
-        $("#popup").removeClass("hidden").addClass("visible");
-        $("#showPopupButton").html('<i class="fa-solid fa-xmark"></i>');
+        toggleShow($("#popup"));
         setTimeout(() => {
             $("#hello-mess").removeClass("hidden").addClass("visible");
         }, 800);
     }
 
-    function hidePopup() {
-        $("#popup").removeClass("visible").addClass("hidden");
-        $("#showPopupButton").html('<i class="message-icon fa-solid fa-comments"></i>');
-    }
-
     function togglePopup() {
         if ($("#popup").hasClass("hidden")) {
             showPopup();
+
             setTimeout(() => {
                 $("#showPopupButton").html('<i class="fa-solid fa-xmark"></i>');
             }, 400);
         } else {
-            hidePopup();
+            if (toggleShow($("#popup"))) {
+                $("#showPopupButton").html('<i class="message-icon fa-solid fa-comments"></i>');
+            }
         }
         if ($("#good-mess").hasClass("visible")) {
             resetMessages();
         }
     }
 
-    function resetMessages() {
-        $("#new-mess").removeClass("visible").addClass("hidden");
-        $("#thanks-mess").removeClass("visible").addClass("hidden");
-        $("#good-mess").removeClass("visible").addClass("hidden");
-        $("#chat-content").removeClass("change-height");
-        $("#start-chat-btn").removeClass("hidden").addClass("visible");
+    function toggleShow(elem) {
+        if (elem.hasClass("visible")) {
+            elem.removeClass("visible").addClass("hidden");
+            return true;
+        } else if (elem.hasClass("hidden")) {
+            elem.removeClass("hidden").addClass("visible");
+        } else {
+            elem.addClass("hidden");
+        }
     }
 
-    setTimeout(showPopup, 10000);
+    function resetMessages() {
+        toggleShow($("#new-mess"));
+        toggleShow($("#thanks-mess"));
+        toggleShow($("#good-mess"));
+        toggleShow($("#start-chat-btn"));
+        $("#chat-content").removeClass("change-height");
+    }
 
     $("#showPopupButton").on("click", togglePopup);
 
     $("#close-btn").on("click", function () {
-        hidePopup();
+        if (toggleShow($("#popup")) == true) {
+            $("#showPopupButton").html('<i class="message-icon fa-solid fa-comments"></i>');
+        }
+
         if ($("#good-mess").hasClass("visible")) {
             resetMessages();
         }
@@ -47,23 +58,26 @@ $(document).ready(function () {
 
     $("#start-chat-btn").on("click", function () {
         setTimeout(function () {
-            $("#new-mess").removeClass("hidden").addClass("visible");
-            $("#start-chat-btn").removeClass("visible");
-            $("#start-chat-btn").addClass("hidden");
-
+            toggleShow($("#new-mess"));
+            toggleShow($("#start-chat-btn"));
             $("#chat-content").addClass("change-height");
         }, 400);
     });
 
     $("#btn-send").on("click", function () {
-        setTimeout(function () {
-            $("#thanks-mess").removeClass("hidden").addClass("visible");
-            $("#chat-content").scrollTop($("#chat-content")[0].scrollHeight);
-
+        if ($("#good-mess").hasClass("hidden")) {
             setTimeout(function () {
-                $("#good-mess").addClass("visible");
+                toggleShow($("#thanks-mess"));
                 $("#chat-content").scrollTop($("#chat-content")[0].scrollHeight);
-            }, 1000);
-        }, 1500);
+
+                setTimeout(function () {
+                    toggleShow($("#good-mess"));
+                    $("#chat-content").scrollTop($("#chat-content")[0].scrollHeight);
+                }, 1000);
+            }, 1500);
+        } else {
+            $("#thanks-mess").addClass("");
+            $("#good-mess").addClass("");
+        }
     });
 });
